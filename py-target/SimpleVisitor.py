c07655c664d50e9f2f9bb59c2bfa81ab5e3d0f09
vanishady <verypeppers@yahoo.com>
2022-12-20 00:10:31 +0100

    visit declstmt

--- py-target/SimpleVisitor.py
+++ py-target/SimpleVisitor.py
@@ -7,21 +7,36 @@ else:
 
 # This class defines a complete generic visitor for a parse tree produced by SimpleParser.
 
+
 class SimpleVisitor(ParseTreeVisitor):
 
     def __init__(self):
+        #create pd output file
         output = open("output.pd", "w")
+        #write first line to pd file
         initline = '#N canvas 676 0 1366 725 12;'
-        output.write(initline+'\n')
+        output.write(initline+'\r\n')
+        #initialize memory as a dict
         self.memory = {}
+        self.posx=0
+        self.posy=0
 
-    def switchnodetype(self, nodetype):
+    def switchnodetype(self, nodetype, x_pos=None, y_pos=None):
+        #based on nodetype, return line to write to pd, including
+        #[[chuncktype x_pos x_pos arg1 arg2 ...]]
         if nodetype == "message":
-            return 'msg 20 30 tryharda;'
+            return f'X msg {x_pos} {y_pos} tryharda'
+        elif nodetype == "object":
+            return f'X obj {x_pos} {y_pos}  *1'
 
     def writetofile(self, line):
         output = open("output.pd", "a")
-        output.write('#X '+ str(line) + '\n')
+        output.write('#'+ str(line) + ';\r\n')
+
+    def positionalg(self):
+        self.posx+= 40
+        self.posy+= 40
+        return self.posx, self.posy
 
         
     # Visit a parse tree produced by SimpleParser#prog.
@@ -46,10 +61,16 @@ class SimpleVisitor(ParseTreeVisitor):
 
     # Visit a parse tree produced by SimpleParser#declarationstmt.
     def visitDeclarationstmt(self, ctx:SimpleParser.DeclarationstmtContext):
+        #node analysis
         name = ctx.ID().getText()
         nodetype = ctx.NODETYPE().getText()
         self.memory[name] = nodetype
-        self.writetofile(self.switchnodetype(nodetype))
+
+        #positional algorithm
+        x,y = self.positionalg()
+
+        #write node to file
+        self.writetofile(self.switchnodetype(nodetype,x,y))
         return self.visitChildren(ctx)
 
 
